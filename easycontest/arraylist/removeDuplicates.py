2f5dca03b0eef00ca5d5f8ee0df7e49643a156c7
lazycat <lkj2015rse@163.com>
2018-09-02 23:38:13 +0800

    刷题的第一天

--- easycontest/arraylist/removeDuplicates.py
+++ easycontest/arraylist/removeDuplicates.py
@@ -1,10 +1,31 @@
 class Solution:
-    def removeDuplicates(self, nums):
+    def maxProfit(self, prices):
         """
-        :type nums: List[int]
+        :type prices: List[int]
         :rtype: int
         """
-        for i in nums:
-            dsfsdfsdfsdfsdf
+        j = 0
+        profit = 0
+        buy = None
+        sell = None
+        for i, v in enumerate(prices[:-1]):
+            j += 1
+            if buy is None and prices[j] > v:
+                buy = v
+            if buy is not None:
+                if prices[j] < v:
+                    sell = v
+                    profit = profit + sell - buy
+                    buy = None
+                    sell = None
+                if sell is None and buy is not None and j == len(prices) - 1:
+                    sell = prices[j]
+                    profit = profit + sell - buy
+                    buy = None
+
+        return profit
 
 
+l = [7, 1, 5, 3, 6, 4]
+print(l[:-1])
+print(Solution().maxProfit(l))
