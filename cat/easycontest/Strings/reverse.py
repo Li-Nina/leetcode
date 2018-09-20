b1496b11f1d24f74ddbe4c4bae4a92782bae0dbe
lazycat <lkj2015rse@163.com>
2018-09-20 22:39:17 +0800

    344. Reverse String and integer

---
+++ cat/easycontest/Strings/reverse.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        x = str(x)
+
+        if x[0] == '-':
+            return int('-' + x[:0:-1]) if -2**31<=int('-' + x[:0:-1])<=2**31-1 else 0
+        else:
+            return int(x[::-1]) if -2**31<=int(x)<=2**31-1 else 0
+
+
+b = -123
+print(Solution().reverse(b))
+
