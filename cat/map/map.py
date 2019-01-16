cdf8b51efa2b4a15cf9acf77ec00a9b48b02c610
lazycat <lkj2015rse@163.com>
2019-01-16 10:09:10 +0800

    数据统计---地图final

--- cat/map/map.py
+++ cat/map/map.py
@@ -13,21 +13,14 @@ def on_click(event):
                       '云南', '浙江', '重庆', '青海', '吉林', '台湾']:
         window.open(event.name + '.html', '_self')
 
-
 def format(w):
     if w.name:
         return w.name + ':' + w.value
     else:
         return 0
 
-
 def return_home():
     window.open('china.html', '_self')
-
-
-# def label_format(w):
-#     return w.name + ':' + w.value
-
 
 def cal_proper_max_value(max_value, split_number):
     if max_value < 1:
@@ -40,9 +33,40 @@ def cal_proper_max_value(max_value, split_number):
 
 
 # 1200*600
-def generate(maptype, attr, value, folder_type):
+# def generate(maptype, attr, value, folder_type):
+#     split_number = 5  # 分段的个数
+#     max_value = cal_proper_max_value(max(value), split_number)
+#     map = Map(maptype, width=1200, height=600)
+#     map.add(
+#         "",
+#         attr,
+#         value,
+#         maptype=maptype,
+#         is_roam=True,
+#         is_label_show=True,
+#         is_map_symbol_show=False,
+#         is_visualmap=True,
+#         visual_range=[0, max_value],  # 最大值必须要大于真实的数值
+#         visual_text_color="#000",
+#         visual_range_color=['#CCFFFF', '#99CCFF', '#336699', '# 003366'],
+#         visual_range_text = [' ', '单位(人天数)'],
+#         # is_piecewise=True,  # 加上后组件为分段型
+#         # visual_split_number=split_number,  # 分段的个数
+#         tooltip_formatter=format,
+#         # label_formatter=label_format, #地图上省份直接显示数据
+#     )
+#     # map.use_theme("shine")
+#     map._option.get("toolbox").get("feature").update(
+#         {
+#             "myTool": {"show": True, "title": "返回全国地图", "icon": home_icon, "onclick": return_home},
+#         }
+#     )
+#     map.on(events.MOUSE_CLICK, on_click)
+#     map.render("./maps/" + folder_type + "/" + maptype + '.html')
+def generate(maptype, attr, value, folder_type, label_name):
     split_number = 5  # 分段的个数
     max_value = cal_proper_max_value(max(value), split_number)
+
     map = Map(maptype, width=1200, height=600)
     map.add(
         "",
@@ -55,8 +79,10 @@ def generate(maptype, attr, value, folder_type):
         is_visualmap=True,
         visual_range=[0, max_value],  # 最大值必须要大于真实的数值
         visual_text_color="#000",
-        is_piecewise=True,  # 加上后组件为分段型
-        visual_split_number=split_number,  # 分段的个数
+        visual_range_color=['#CCFFFF', '#99CCFF', '#336699', '# 003366'],
+        visual_range_text = [' ', label_name],
+        # is_piecewise=True,  # 加上后组件为分段型
+        # visual_split_number=split_number,  # 分段的个数
         tooltip_formatter=format,
         # label_formatter=label_format, #地图上省份直接显示数据
     )
@@ -73,12 +99,12 @@ def generate(maptype, attr, value, folder_type):
 if __name__ == '__main__':
     province_name, province_value = Geo.cast(data.province_day_data)
     city_name, city_value = Geo.cast(data.city_day_data)
-    generate('china', province_name, province_value, 'days')
+    generate('china', province_name, province_value, 'days', '单位(人天数)')
     for province in province_name:
-        generate(province, city_name, city_value, 'days')
+        generate(province, city_name, city_value, 'days', '单位(人天数)')
 
     province_name, province_value = Geo.cast(data.province_price_data)
     city_name, city_value = Geo.cast(data.city_price_data)
-    generate('china', province_name, province_value, 'prices')
+    generate('china', province_name, province_value, 'prices','单位(万元)')
     for province in province_name:
-        generate(province, city_name, city_value, 'prices')
+        generate(province, city_name, city_value, 'prices','单位(万元)')
