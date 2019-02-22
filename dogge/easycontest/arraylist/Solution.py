e5210ca632e56db3cb2e2235dfdbdc3455a29d04
xiaomage <mazheng_2008ok@163.com>
2019-02-22 15:24:52 +0800

    dp problems

--- dogge/easycontest/arraylist/Solution.py
+++ dogge/easycontest/arraylist/Solution.py
@@ -314,6 +314,47 @@ class Solution:
         node.right = self.sortedArrayToBST(nums[mid + 1:])
         return node
 
+    res_dict = {}
+
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        else:
+            n_1 = self.res_dict.get(n - 1) if self.res_dict.get(n - 1) else self.climbStairs(n - 1)
+            n_2 = self.res_dict.get(n - 2) if self.res_dict.get(n - 2) else self.climbStairs(n - 2)
+            self.res_dict[n - 1] = n_1
+            self.res_dict[n - 2] = n_2
+            return n_1 + n_2
+
+    def maxProfit(self, prices: 'List[int]') -> 'int':
+        max_profit = 0
+        if prices:
+            min_price = prices[0]
+            for i in prices:
+                if min_price > i:
+                    min_price = i
+                elif i - min_price > max_profit:
+                    max_profit = i - min_price
+        return max_profit
+
+    def maxSubArray(self, nums: 'List[int]') -> 'int':
+        """
+        Analysis of this problem:
+        Apparently, this is a optimization problem, which can be usually solved by DP. So when it comes to DP, the first thing for us to figure out is the format of the sub problem(or the state of each sub problem). The format of the sub problem can be helpful when we are trying to come up with the recursive relation.
+        At first, I think the sub problem should look like: maxSubArray(int A[], int i, int j), which means the maxSubArray for A[i: j]. In this way, our goal is to figure out what maxSubArray(A, 0, A.length - 1) is. However, if we define the format of the sub problem in this way, it's hard to find the connection from the sub problem to the original problem(at least for me). In other words, I can't find a way to divided the original problem into the sub problems and use the solutions of the sub problems to somehow create the solution of the original one.
+        So I change the format of the sub problem into something like: maxSubArray(int A[], int i), which means the maxSubArray for A[0:i ] which must has A[i] as the end element. Note that now the sub problem's format is less flexible and less powerful than the previous one because there's a limitation that A[i] should be contained in that sequence and we have to keep track of each solution of the sub problem to update the global optimal value. However, now the connect between the sub problem & the original one becomes clearer:
+        maxSubArray(A, i) = maxSubArray(A, i - 1) > 0 ? maxSubArray(A, i - 1) + A[i] : A[i] ;
+        """
+        for i in range(1, len(nums)):
+            nums[i] = nums[i - 1] + nums[i] if nums[i - 1] > 0 else nums[i]
+        return max(nums)
+
 
 # Definition for a binary tree node.
 class TreeNode(object):
@@ -342,8 +383,3 @@ class ListNode:
 
 if __name__ == '__main__':
     pass
-l = [1, 2, 3, 4, 5]
-mid = (len(l) - 1) // 2
-print(l[mid])
-print(l[0:mid])
-print(l[mid + 1:])
