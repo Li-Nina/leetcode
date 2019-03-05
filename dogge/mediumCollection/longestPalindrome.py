0ab203cd987496c1d6ffbf43b026de7195dd72fe
xiaomage <mazheng_2008ok@163.com>
2019-03-05 17:51:45 +0800

    longestPalindrome dp方法的一些改进

--- dogge/mediumCollection/longestPalindrome.py
+++ dogge/mediumCollection/longestPalindrome.py
@@ -1,5 +1,5 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
+    def longestPalindrome_origin(self, s: str) -> str:
         length = len(s)
         dp = {}
         max_len = 1
@@ -23,8 +23,49 @@ class Solution:
 
         return s[start:start + max_len]
 
+    def longestPalindrome2(self, s: str) -> str:
+        length = len(s)
+        dp = {}
+        max_len = 1
+        start = 0
+
+        for _len in range(2, length + 1):
+            i = 0
+            while i <= length - _len:
+                j = i + _len - 1
+                if _len <= 3 and s[i] == s[j]:
+                    dp[(i, j)] = True
+                    if _len > max_len:
+                        max_len = _len
+                        start = i
+                elif s[i] == s[j] and dp.get((i + 1, j - 1)):
+                    dp[(i, j)] = True
+                    if _len > max_len:
+                        max_len = _len
+                        start = i
+                i += 1
+        return s[start:start + max_len]
+
+    def longestPalindrome(self, s: str) -> str:
+        length = len(s)
+        dp = {}
+        max_len = 1
+        start = 0
+
+        for _len in range(2, length + 1):
+            i = 0
+            while i <= length - _len:
+                j = i + _len - 1
+                if (_len <= 3 and s[i] == s[j]) or (s[i] == s[j] and dp.get((i + 1, j - 1))):
+                    dp[(i, j)] = True
+                    if _len > max_len:
+                        max_len = _len
+                        start = i
+                i += 1
+        return s[start:start + max_len]
+
 
 if __name__ == '__main__':
-    s = "babad"
+    s = "abcda"
     x = Solution().longestPalindrome(s)
     print(x)
