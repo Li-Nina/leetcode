faec4a44718cfd574da1251380af3e8a880c9c4c
xiaomage <MAzheng5115>
2019-03-14 23:11:04 +0800

    generateParenthesis

--- dogge/mediumCollection/generateParenthesis.py
+++ dogge/mediumCollection/generateParenthesis.py
@@ -1,4 +1,21 @@
 class Solution:
     def generateParenthesis(self, n: int) -> 'List[str]':
-        lbracket_left = n
-        rbracket_left = n
+        def dfsHelper(combination, lbracket_left, rbracket_left):
+            if lbracket_left == 0 and rbracket_left == 0:
+                rst.append(combination)
+            elif lbracket_left == 0 and rbracket_left > 0:
+                dfsHelper(combination + ")", lbracket_left, rbracket_left - 1)
+            elif lbracket_left < rbracket_left:
+                dfsHelper(combination + "(", lbracket_left - 1, rbracket_left)
+                dfsHelper(combination + ")", lbracket_left, rbracket_left - 1)
+            else:
+                dfsHelper(combination + "(", lbracket_left - 1, rbracket_left)
+
+        rst = []
+        dfsHelper("", n, n)
+        return rst
+
+
+if __name__ == '__main__':
+    x = Solution().generateParenthesis(4)
+    print(x)
