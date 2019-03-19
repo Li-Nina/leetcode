9c6d6b511acb2c16699c98bef3534dd6299aa5cd
xiaomage <mazheng_2008ok@163.com>
2019-03-19 11:17:47 +0800

    subsets

--- dogge/mediumCollection/subsets.py
+++ dogge/mediumCollection/subsets.py
@@ -1,5 +1,28 @@
 class Solution:
-    def subsets(self, nums: 'List[int]') -> 'List[List[int]]':
-        rst=[]
-        def backtracking()
+    """
+                 temp[  ]
+             /      |     \
+           [1]     [2]    [3]
+         /    \     |
+      [1,2] [1,3] [2,3]
+       /
+    [1,2,3]
+    """
 
+    def subsets(self, nums: 'List[int]') -> 'List[List[int]]':
+        rst = []
+
+        def backtracking(temp, index):
+            rst.append(temp[:])
+            for i in range(index, len(nums)):
+                temp.append(nums[i])
+                backtracking(temp, i + 1)
+                temp.pop()
+
+        backtracking([], 0)
+        return rst
+
+
+if __name__ == '__main__':
+    r = Solution().subsets([1, 2, 3])
+    print(r)
