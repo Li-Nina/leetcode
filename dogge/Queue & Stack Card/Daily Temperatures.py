810ec140a7c99e9818d94a36bce7a6fd02e57a77
xiaomage <mazheng_2008ok@163.com>
2019-04-25 13:17:01 +0800

    Daily Temperatures

--- dogge/Queue & Stack Card/Daily Temperatures.py
+++ dogge/Queue & Stack Card/Daily Temperatures.py
@@ -1,27 +1,40 @@
-class Solution:
-    def dailyTemperatures(self, T: 'List[int]') -> 'List[int]':
-        if not T:
-            return []
-        rst = [0 for _ in range(len(T))]
-        stack = [T[0]]
-        low = T[0]
+import sys
 
-        for i in range(1, len(T)):
-            if T[i] <= low:
-                stack.append(T[i])
-                low = T[i]
-            else:
-                count = 1
-                while stack:
-                    stack.pop()
-                    if rst[i - count] == 0:
-                        rst[i - count] = count
-                    count += 1
-                stack.append(T[i])
-                low = T[i]
+
+class Solution:
+    def dailyTemperatures_inverse(self, T: 'List[int]') -> 'List[int]':
+        """
+        The length of temperatures will be in the range [1, 30000].
+        Each temperature will be an integer in the range [30, 100].
+        rst={val:minIndex}
+        """
+        nst = [sys.maxsize for _ in range(102)]
+        rst = [0 for _ in range(len(T))]
+        for i in range(len(T) - 1, -1, -1):
+            warmer_index = min(j for j in nst[T[i] + 1: 102])
+            if warmer_index < sys.maxsize:
+                rst[i] = warmer_index - i
+            nst[T[i]] = i
+        return rst
+
+    def dailyTemperatures_stack(self, T: 'List[int]') -> 'List[int]':
+        """
+        The length of temperatures will be in the range [1, 30000].
+        Each temperature will be an integer in the range [30, 100].
+        """
+        rst = [0 for _ in range(len(T))]
+        stack = []
+
+        for i, v in enumerate(T):
+            while stack and stack[-1][0] < v:
+                v_, i_ = stack.pop()
+                rst[i_] = i - i_
+            stack.append((v, i))
         return rst
 
 
 if __name__ == '__main__':
-    x = Solution().dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])
+    x = Solution().dailyTemperatures_inverse([89, 62, 70, 58, 47, 47, 46, 76, 100, 70])
+    print(x)
+    x = Solution().dailyTemperatures_stack([89, 62, 70, 58, 47, 47, 46, 76, 100, 70])
     print(x)
