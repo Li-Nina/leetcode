bb95b66c7bea3af1bb2ca081aecd74d12594482f
xiaomage <mazheng_2008ok@163.com>
2019-05-05 15:34:09 +0800

    Binary Tree 2 problems

--- dogge/Binary Tree Card/Lowest Common Ancestor of a Binary Tree.py
+++ dogge/Binary Tree Card/Lowest Common Ancestor of a Binary Tree.py
@@ -8,4 +8,40 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        pass
+        """
+        Our base cases.
+        How our recursion stops.
+        When we have an answer.
+
+        1.) If the node we are holding is null then we can't search...just return null
+        2.) If we find either value return ourselves to the caller
+        Remember, we are "grabbing"/"holding" 'root' in this call.
+        """
+        if not root or root == p or root == q:
+            return root
+        """
+        'root' doesn't satisfy any of our base cases.
+        Search left and then search right.
+        """
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left is None:
+            """
+            If nothing turned up on the left, return whatever we got
+            back on the right
+            """
+            return right
+        elif right is None:
+            """
+            If nothing turned up on the right, return whatever we got
+            back on the left.
+            """
+            return left
+        else:
+            """
+            If we reach here that means we got something back on the left AND
+            right...that means this node is the LCA...because our recursion returns
+            from bottom to up...so we return what we hold...'root'
+            """
+            return root
