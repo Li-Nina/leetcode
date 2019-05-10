dd658106b8f4491d133b49f903c5d4005e9f3a6a
xiaomage <mazheng_2008ok@163.com>
2019-05-10 10:27:11 +0800

    Merge Two Sorted Lists recursive approach

--- dogge/Recursion Card/Merge Two Sorted Lists.py
+++ dogge/Recursion Card/Merge Two Sorted Lists.py
@@ -44,3 +44,15 @@ class Solution:
         else:
             after.next = self.mergeTwoLists(n, m)
         return node
+
+    def mergeTwoLists_concise(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
